ec89caa5d23f6818c984e2099df09eeef4887269
rizwans981 <rizwans981@gmail.com>
2015-04-21 10:39:50 +0530

    first commit

---
+++ Desktop/demo/demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by Shaikh Rizwan on 3/23/15.
+//  Copyright (c) 2015 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
